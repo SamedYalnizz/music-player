78f314cc965d34c69a6935e225867be2b4c9b030
Samed Yalniz <yalniz.samed@gmail.com>
2020-07-20 07:15:40 +0200

    adding dynamic progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -70,7 +72,6 @@ function prevSong() {
     if(songIndex < 0){
         songIndex = songs.length - 1; 
     }
-    console.log(songIndex);
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -81,7 +82,6 @@ function nextSong() {
     if(songIndex > songs.length -1){
         songIndex = 0; 
     }
-    console.log(songIndex);
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -89,6 +89,19 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar & Time
+function updateProgressBar(event) {
+    if (isPlaying) {
+        const {duration, currentTime} = event.srcElement;
+        console.log(duration, currentTime);
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate',updateProgressBar);
